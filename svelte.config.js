4015052ed027fdfd1904b6459afa90a200ec194a
Karolis Šarapnickis <karolis.sarapnickis@nordsec.com>
2021-01-19 11:57:16 +0200

    fix: updat estuff

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,9 @@ module.exports = {
     },
     postcss: true,
   }),
+  compilerOptions: {
+    css: false,
+  },
   kit: {
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
